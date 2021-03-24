9254d2b7195bcbf834cc629be0df275cdee66c32
ynssynk <ynssynk@hotmail.com>
2021-03-24 21:11:06 +0300

    exception handling,reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{HttpClientModule} from '@angular/common/http'
-import{FormsModule} from '@angular/forms'
+import{FormsModule,ReactiveFormsModule} from '@angular/forms'
 import{BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +13,9 @@ import { VatAddedPipePipe } from './pipes/vat-added-pipe.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import{ToastrModule} from 'ngx-toastr';
-import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -23,13 +25,16 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     NaviComponent,
     VatAddedPipePipe,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    ProductAddComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
